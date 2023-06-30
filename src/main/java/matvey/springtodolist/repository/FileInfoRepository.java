7231f53961ed5e1b944a0b0ecaf0d3b1757c8601
KMatveychikov <matv00@gmail.com>
2023-06-30 14:32:12 +0300

    update

--- src/main/java/matvey/springtodolist/repository/FileInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package matvey.springtodolist.repository;
-
-import matvey.springtodolist.model.FileInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FileInfoRepository extends MongoRepository<FileInfo, String> {
-}
